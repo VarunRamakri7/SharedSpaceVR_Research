be01e00fb138dcfbfb462f8c4f4a7dac55079d3c
Varun Ramakrishnan <oceans.varun@gmail.com>
2019-09-22 18:51:14 -0400

    Added Path Generating
    Modified S_SpawnOnClick to generate 10 cubes, also added script to check for collision.
    Added tags.

---
+++ SharedSpace_Research/Assets/Scripts/S_CheckGrid.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_CheckGrid : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if ((collision.gameObject.tag == "obstacle") || (collision.gameObject.tag == "Path"))
+        {
+            Debug.Log("Collision Detected: " + collision.gameObject.name);
+            this.gameObject.transform.position = new Vector3(Random.Range(-21.0f, 21.0f), 2.6f, Random.Range(-21.0f, 21.0f));
+        }
+    }
+}
--- SharedSpace_Research/Assets/Scripts/S_SpawnOnCick.cs
+++ SharedSpace_Research/Assets/Scripts/S_SpawnOnCick.cs
@@ -5,14 +5,29 @@ using UnityEngine;
 public class S_SpawnOnCick : MonoBehaviour
 {
     public new GameObject gameObject;
-    //public Transform Spawn;
+    private GameObject cube; // Random cube
+    private int numberCube = 0;
+    private Vector3 oldPosition;
 
     Ray ray; // Initialise the ray
     RaycastHit hit; // Initialise the hit
 
+    void Start()
+    {
+        oldPosition = new Vector3(Random.Range(-21.0f, 21.0f), 2.5f, Random.Range(-21.0f, 21.0f));
+    }
+
     // Update is called once per frame
     void Update()
     {
+        // Generate 10 cubes
+        if(numberCube < 10)
+        {
+            generateCube();
+            numberCube++;
+        }
+
+        // Get the mouse-click location
         ray = Camera.main.ScreenPointToRay(Input.mousePosition); // Ray will go from center of main camera to mouse
         
         // Get all info if the ray hits something
@@ -20,10 +35,36 @@ public class S_SpawnOnCick : MonoBehaviour
         {
             if(Input.GetMouseButtonDown(0))
             {
-                //hit.point.y = new Vector3(0, hit.point.y + 2, 0);
+                // Place a new object at the hit point
                 Instantiate(gameObject, new Vector3(hit.point.x, hit.point.y + 0.5f, hit.point.z), Quaternion.identity); // Instantiate prefab on point of hit
-                Debug.Log(hit.point);
+                Debug.Log("Object placed at: " + hit.point);
             }
         }
+    }
+
+    // Generate a cube at a random position
+    void generateCube()
+    {
+        cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.tag = "Path";
+        cube.AddComponent<S_CheckGrid>();
+        cube.AddComponent<Rigidbody>();
+        cube.GetComponent<Rigidbody>().isKinematic = false;
+        cube.transform.position = oldPosition;
+        cube.transform.localScale = new Vector3(2, 2, 2);
+
+        float randFloat = Random.Range(1.6f, 2.5f); // Random increment for the X-Coordinate
+
+        // Change the position for the next cube
+        if (cube.transform.position.x - randFloat > -21)
+        {
+            oldPosition = new Vector3(cube.transform.position.x - randFloat, 2.5f, Random.Range(-21.0f, 21.0f));
+        }
+        else if (cube.transform.position.x + randFloat < 21)
+        {
+            oldPosition = new Vector3(cube.transform.position.x + randFloat, 2.5f, Random.Range(-21.0f, 21.0f));
+        }
+
+        Debug.Log("Cube generated");
     }
 }
